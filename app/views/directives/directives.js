5902d0692e2db6f1029888d2bd80868cbf503ac6
cyberabis <babishek@gmail.com>
2015-08-02 10:32:55 +0530

    ui enhancements

--- app/views/directives/directives.js
+++ app/views/directives/directives.js
@@ -26,7 +26,7 @@ define(['angular',
             replace:true,
             scope: {},
             //template: '<input id="google_places_ac" name="google_places_ac" type="text" class="input-block-level" placeholder="Search by Map Location" style="border :2px solid rgba(59, 153, 252, 0.68); width:100%; position: relative; z-index:3;"/>',
-            template: '<input id="google_places_ac" name="google_places_ac" type="text" class="form-control" placeholder="Search by Map Location" />',
+            template: '<input id="google_places_ac" name="google_places_ac" type="text" class="form-control tm-search-text" placeholder="Search by location" />',
             link: function($scope, elm, attrs){
                 var autocomplete = new google.maps.places.Autocomplete($("#google_places_ac")[0], {});
                 google.maps.event.addListener(autocomplete, 'place_changed', function() {
