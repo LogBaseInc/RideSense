b828622ea74f508a2974c95fc5661556570d2300
kalaivanilogbase <kalaivani@logbase.io>
2016-01-23 15:36:43 +0530

    Token

--- app/views/account/detail/detail.js
+++ app/views/account/detail/detail.js
@@ -7,6 +7,8 @@ define(['angular',
         function accountdetail($scope, $location, config, notify, spinner, sessionservice, utility) {
             var submitted = false;
             var vm = this;
+            var accountid = sessionservice.getaccountId();
+            vm.token = {};
 
             Object.defineProperty(vm, 'canupdate', {
                 get: canupdate
@@ -16,14 +18,13 @@ define(['angular',
                 return submitted || field.$dirty;
             };
 
-            var accountref = new Firebase(config.firebaseUrl+'accounts/'+sessionservice.getaccountId()+'/name');
-            var emailref = new Firebase(config.firebaseUrl+'accounts/'+sessionservice.getaccountId()+'/email');
-            var addressref = new Firebase(config.firebaseUrl+'accounts/'+sessionservice.getaccountId()+'/address');
+            var accountref = new Firebase(config.firebaseUrl+'accounts/'+accountid+'/name');
+            var addressref = new Firebase(config.firebaseUrl+'accounts/'+accountid+'/address');
+            var tokenref = new Firebase(config.firebaseUrl+'accounts/'+accountid+'/settings/token');
 
             activate();
 
             function activate(){
-                
                 spinner.show();
                 accountref.once("value", function(snapshot) {
                     vm.accountname = snapshot.val();
@@ -32,6 +33,7 @@ define(['angular',
                     utility.errorlog("The account name read failed: ", errorObject);
                 });
 
+                var emailref = new Firebase(config.firebaseUrl+'accounts/'+accountid+'/email');
                 emailref.once("value", function(snapshot) {
                     vm.email = snapshot.val();
                     utility.applyscope($scope);
@@ -48,6 +50,15 @@ define(['angular',
                 }, function (errorObject) {
                     utility.errorlog("The address read failed: ", errorObject);
                 });
+
+                tokenref.on("value", function(snapshot) {
+                    vm.token = snapshot.val();
+                    vm.token.createdon = moment(vm.token.createdon).format("MMM DD, YYYY hh:mm A");
+                    spinner.hide();
+                    utility.applyscope($scope);
+                }, function (errorObject) {
+                    utility.errorlog("The setting token read failed: ", errorObject);
+                });
             }
 
             function canupdate(){
@@ -58,6 +69,18 @@ define(['angular',
                 accountref.set(vm.accountname);
                 addressref.set(vm.address);
                 notify.success('Account details updated successfully');
+            }
+
+            vm.getToken = function() {
+                var token = utility.generateUUID();
+                var date = moment(new Date()).format("YYYY/MM/DD HH:mm:ss");
+                tokenref.set({id: token, createdon: date});
+
+                var tokensref = new Firebase(config.firebaseUrl+'tokens/'+token);
+                var data = {};
+                data.accountId = accountid;
+                data.orderCount = {count: 0, date: moment(new Date()).format("YYYYMMDD")};
+                tokensref.set(data);
             }
         }
     })();
